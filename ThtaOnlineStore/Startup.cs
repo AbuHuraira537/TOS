b708c45910d7dfc9e06204fab58369634a5948af
AbuHuraira537 <abouhuraira967@gmail.com>
2020-05-13 12:41:45 +0500

    First 9 tasks

--- ThtaOnlineStore/Startup.cs
+++ ThtaOnlineStore/Startup.cs
@@ -67,7 +67,7 @@ namespace ThtaOnlineStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=SystemUsers}/{action=Register}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
